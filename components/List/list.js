3e9da05c4d3377ed22cd7cc357f111b34fd48370
srinibas <srinibaskhuntia39@gmail.com>
2022-02-05 16:45:55 +0530

    List done

---
+++ components/List/list.js
@@ -0,0 +1,45 @@
+// const navIcon=document.querySelector(".nav-icon");
+// const sidebar=document.querySelector(".sidebar");
+// const homepage=document.querySelector(".homepage");
+
+
+
+// const handleSize=()=>{
+    
+//     // if(window.screen.width<="710px"){
+//     //     // navIcon.style.display="inline";
+//     //     console.log("Hello");
+//     // }
+//     // else{
+//     //     navIcon.style.display="none";
+//     // }
+//     if(window.screen.width<710){
+//         navIcon.style.display="flex";
+//         sidebar.style.display="none";
+//     }
+//     else{
+//         navIcon.style.display="none"
+//         sidebar.style.display="block";
+//     }
+// }
+
+// const handleNavClick=()=>{
+//      if(sidebar.style.display!=="block" ){
+//         sidebar.style.display="block"
+//     }
+//     else if(sidebar.style.display!=="none"){
+//        sidebar.style.display="none" ;
+//    }
+// }
+
+// const handleScroll=()=>{
+//     if(homepage.scrollTop > 50){
+
+//         sidebar.style.display="none" ;
+//     }
+// }
+
+
+window.addEventListener("resize",handleSize);
+// navIcon.addEventListener("click",handleNavClick);
+
