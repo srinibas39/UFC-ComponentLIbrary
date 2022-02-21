18ed0888a0be79dd91af20d9502b9873ac18f1fb
srinibas <srinibaskhuntia39@gmail.com>
2022-02-21 22:34:22 +0530

    Drawer done

--- components/Drawer/Drawer.js
+++ components/Drawer/Drawer.js
@@ -1,16 +1,16 @@
 
-let sidebar=document.querySelector(".aside-drawer");
+let sidebar2=document.querySelector(".aside-drawer");
 let navopen=document.querySelector(".navopen");
 
 
 navopen.addEventListener("click",()=>{
-    if(sidebar.style.display!=="none"){
+    if(sidebar2.style.display!=="none"){
         
-        sidebar.style.display="none";
+        sidebar2.style.display="none";
         
     }
     else{
-       sidebar.style.display="flex";
+       sidebar2.style.display="flex";
        
     }
 })
